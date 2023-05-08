84676e0d94df84de1f0f6172def17e28f8c94344
Antibiotic <ofsposs@gmail.com>
2023-05-08 17:51:35 +0200

    Update main.py

--- console_bot/main.py
+++ console_bot/main.py
@@ -1,13 +1,40 @@
 import console_bot.ab_work as ab
-from console_bot.handlers import no_command, instruction
+from console_bot.handlers import no_command, instruction, show_all
 import console_bot.Notes as Notes
 import console_bot.sort as sort
+import abc
 
 MAIN_INSTRUCTION = 'instruction for menu.txt'
 
 
-def help(*args, **kwargs):
-    return instruction(MAIN_INSTRUCTION)
+class UserInterface(abc.ABC):
+
+    @abc.abstractmethod
+    def show_help(self):
+        pass
+
+    @abc.abstractmethod
+    def show_contacts(self):
+        pass
+
+    @abc.abstractmethod
+    def show_notes(self):
+        pass
+
+
+class ConsoleInterface(UserInterface):
+
+    def show_help(self):
+        return instruction(MAIN_INSTRUCTION)
+
+    def show_contacts(self):
+        return show_all()
+
+    def show_notes(self):
+        return Notes.all("")
+
+
+console_i = ConsoleInterface()
 
 
 def good_bye(*args, **kwargs):
@@ -15,10 +42,12 @@ def good_bye(*args, **kwargs):
 
 
 MAIN_COMMANDS = {
-    'help': help,
+    'help': console_i.show_help,
     'address book': ab.main,
     'notes': Notes.main,
     'sort': sort.main,
+    'show contacts': console_i.show_contacts,
+    'show notes': console_i.show_notes,
     'exit': good_bye
 }
 
@@ -27,9 +56,9 @@ MAIN_COMMANDS_WORDS = '|'.join(MAIN_COMMANDS)
 
 
 def main():
-    print(help())
+    print(console_i.show_help())
     while True:
-        user_input = input('Choose points: address book, notes, sort: ')
+        user_input = input('Choose points: address book, notes, show contacts, show notes, sort: ')
         command, _ = ab.parser(user_input, MAIN_COMMANDS_WORDS)
         handler = MAIN_COMMANDS.get(command, no_command)
         output = handler()
